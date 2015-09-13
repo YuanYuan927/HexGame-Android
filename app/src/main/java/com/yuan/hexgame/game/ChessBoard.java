5b5136ad4304420d8a3c973ab734ab2aa9df1165
YuanSun <yuansun927@gmail.com>
2015-09-13 19:34:35 +0800

    Add the rule to determine who win the game

--- app/src/main/java/com/yuan/hexgame/game/ChessBoard.java
+++ app/src/main/java/com/yuan/hexgame/game/ChessBoard.java
@@ -16,27 +16,29 @@ public class ChessBoard implements Board {
     private static final char EMPTY = 'O';
 
     private Player[] mBoard;
-    private int num;
+    private int mNum; // The amount of hex pieces.
+    private int mN;   // The board size is NxN
     private Player mFirstPlayer;
     private Player mCurrentPlayer;
 
     private UnionFind mUFA, mUFB;
 
     public ChessBoard(int n, Player firstPlayer) {
-        this.num = n * n;
-        mBoard = new Player[num + 1];
+        this.mN = n;
+        this.mNum = n * n;
+        mBoard = new Player[mNum + 1];
 
-        mUFA = new WeightedQuickUnionPathCompressionUF(num + 2);
-        mUFB = new WeightedQuickUnionPathCompressionUF(num + 2);
+        mUFA = new WeightedQuickUnionPathCompressionUF(mNum + 2);
+        mUFB = new WeightedQuickUnionPathCompressionUF(mNum + 2);
         for (int i = 0; i < n; i++) {
             mUFA.union(0, i * n + 1);
-            mUFA.union(n + 1, (i + 1) * n);
+            mUFA.union(mNum + 1, (i + 1) * n);
             mUFB.union(0, i + 1);
-            mUFB.union(n + 1, (n - 1) * n + i + 1);
+            mUFB.union(mNum + 1, (n - 1) * n + i + 1);
         }
 
         mFirstPlayer = firstPlayer;
-        mCurrentPlayer = firstPlayer;
+        mCurrentPlayer = firstPlayer == Player.A ? Player.B : Player.A;
     }
 
     @Override
@@ -57,6 +59,115 @@ public class ChessBoard implements Board {
             return;
         mBoard[i] = player;
         mCurrentPlayer = player;
+        int row = (i - 1) / mN;
+        int col = (i - 1) % mN;
+        UnionFind uf = player == Player.A ? mUFA : mUFB;
+        if (i == 1) { // top-left
+            if (mBoard[i + 1] == player) {
+                uf.union(i, i + 1);
+            }
+            if (mBoard[i + mN] == player) {
+                uf.union(i, i + mN);
+            }
+        } else if (i == mNum) { // bottom-right
+            if (mBoard[i - 1] == player) {
+                uf.union(i, i - 1);
+            }
+            if (mBoard[i - mN] == player) {
+                uf.union(i, i - mN);
+            }
+        } else if (i == mN) { // top-right
+            if (mBoard[i - 1] == player) {
+                uf.union(i, i - 1);
+            }
+            if (mBoard[i + mN - 1] == player) {
+                uf.union(i, i + mN - 1);
+            }
+            if (mBoard[i + mN] == player) {
+                uf.union(i, i + mN);
+            }
+        } else if (i == mNum - mN + 1) { // bottom-left
+            if (mBoard[i - 1] == player) {
+                uf.union(i, i - 1);
+            }
+            if (mBoard[i + mN - 1] == player) {
+                uf.union(i, i + mN - 1);
+            }
+            if (mBoard[i + mN] == player) {
+                uf.union(i, i + mN);
+            }
+        } else if (row == 0) { // top
+            if (mBoard[i - 1] == player) {
+                uf.union(i, i - 1);
+            }
+            if (mBoard[i + 1] == player) {
+                uf.union(i, i + 1);
+            }
+            if (mBoard[i + mN] == player) {
+                uf.union(i, i + mN);
+            }
+            if (mBoard[i + mN + 1] == player) {
+                uf.union(i, i + mN + 1);
+            }
+        } else if (row == mN - 1) { // bottom
+            if (mBoard[i - 1] == player) {
+                uf.union(i, i - 1);
+            }
+            if (mBoard[i + 1] == player) {
+                uf.union(i, i + 1);
+            }
+            if (mBoard[i - mN] == player) {
+                uf.union(i, i - mN);
+            }
+            if (mBoard[i - mN + 1] == player) {
+                uf.union(i, i - mN + 1);
+            }
+        } else if (col == 0) { // left
+            if (mBoard[i - mN] == player) {
+                uf.union(i, i - mN);
+            }
+            if (mBoard[i + mN] == player) {
+                uf.union(i, i + mN);
+            }
+            if (mBoard[i - mN + 1] == player) {
+                uf.union(i, i - mN + 1);
+            }
+            if (mBoard[i + 1] == player) {
+                uf.union(i, i + 1);
+            }
+        } else if (col == mN - 1) { // right
+            if (mBoard[i - mN] == player) {
+                uf.union(i, i - mN);
+            }
+            if (mBoard[i + mN] == player) {
+                uf.union(i, i + mN);
+            }
+            if (mBoard[i - mN - 1] == player) {
+                uf.union(i, i - mN - 1);
+            }
+            if (mBoard[i - 1] == player) {
+                uf.union(i, i - 1);
+            }
+        } else {
+            if (mBoard[i - mN] == player) {
+                uf.union(i, i - mN);
+            }
+            if (mBoard[i + mN] == player) {
+                uf.union(i, i + mN);
+            }
+            if (mBoard[i - mN + 1] == player) {
+                uf.union(i, i - mN + 1);
+            }
+            if (mBoard[i + mN - 1] == player) {
+                uf.union(i, i + mN - 1);
+            }
+            if (mBoard[i - 1] == player) {
+                uf.union(i, i - 1);
+            }
+            if (mBoard[i + 1] == player) {
+                uf.union(i, i + 1);
+            }
+        }
     }
 
     @Override
@@ -73,17 +184,17 @@ public class ChessBoard implements Board {
 
     @Override
     public boolean isAWin() {
-        return mUFA.isConnected(0, num + 1);
+        return mUFA.isConnected(0, mNum + 1);
     }
 
     @Override
     public boolean isBWin() {
-        return mUFB.isConnected(0, num + 1);
+        return mUFB.isConnected(0, mNum + 1);
     }
 
     private void validate(int i) {
-        if (i < 1 || i > num) {
-            throw new IndexOutOfBoundsException("Index " + i + " is not between 1 and " + num);
+        if (i < 1 || i > mNum) {
+            throw new IndexOutOfBoundsException("Index " + i + " is not between 1 and " + mNum);
         }
     }
 }
